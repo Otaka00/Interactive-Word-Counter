d8bb13f24dc54dffad14a2ddb62b17f67e6ff177
Ahmad Ossama <69368484+Otaka00@users.noreply.github.com>
2023-12-17 17:06:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function countWords() {
+    // Get the input text from the textarea and split the text into an array of words
+
+    var words = document.getElementById("inputText").textContent.trim().split(/\s+/);
+    console.log(words);
+
+   //Filter out empty strings
+    words = words.filter(function(word) {
+        return word.length > 0;
+    });
+
+    var longestWord = findLongestWord(words);
+    console.log(longestWord);
+    var wordCountElement = document.getElementById("wordCount");
+    wordCountElement.innerHTML = "Words Count: " + words.length;
+     
+    // Highlight the longest word
+    const regex = new RegExp(longestWord,'g');
+    const highlighted = document.getElementById("inputText").textContent.replace(regex, `<span id="longestWord"> ${regex} </span>`);
+    inputText.innerHTML = highlighted;
+
+}
+function removeHighlights() {
+   
+    const textContainer = document.getElementById('inputText');
+    const spans = textContainer.querySelectorAll('#longestWord');
+    spans.forEach(span => {
+      span.outerHTML = span.innerHTML;
+    });
+    
+  }
+  
+  function findLongestWord(words) {
+    var longestWord = words[0];
+    for (var i = 1; i < words.length; i++) {
+        if (words[i].length > longestWord.length) 
+            longestWord = words[i];
+    }
+    return longestWord;
+}
+// function highlightText(words){
+//     var longestword = findLongestWord(words);
+//     const regex = new RegExp(longestword,'g');
+//      const highlighted = document.getElementById("inputText").textContent.replace(regex, `<span id="longestWord"> ${regex} </span>`);
+//      console.log(highlighted);
+//      inputText.innerHTML = highlighted;
+// }
+   
